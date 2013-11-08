f5c764eb12ce636b254c88196aa71800357e7058
Luke Campbell <luke.s.campbell@gmail.com>
2013-11-08 09:46:00 -0500

    Made into organizer package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(  name='organizer', 
+        version='0.0.1',
+        description='Something to organize my life',
+        author='Luke Campbell',
+        author_email='luke.s.campbell@gmail.com',
+        url='https://github.com/lukecampbell/organizer/',
+        packages=packages,
+        package_data={'organizer' : ['organizer/config/*.yml']},
+        install_requires=['sqlalchemy']
+        )
+
+
